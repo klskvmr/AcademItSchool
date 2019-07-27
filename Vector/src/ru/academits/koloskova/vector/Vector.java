c1e43e4894b99775209e31aaa2fa9b8238be25c1
klskvmr <koloskova.marii@gmail.com>
2019-07-27 21:54:51 +0700

    add vector

--- Vector/src/ru/academits/koloskova/vector/Vector.java
+++ Vector/src/ru/academits/koloskova/vector/Vector.java
@@ -1,4 +1,58 @@
 package ru.academits.koloskova.vector;
 
 public class Vector {
+    private int n;
+    private double[] valuesArray;
+
+    public Vector() {
+
+    }
+
+    public Vector(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n can’t be <= 0");
+        }
+        this.n = n;
+
+        double[] valuesArray = new double[n];
+        for (int i = 0; i < n; i++) {
+            valuesArray[i] = 0;
+        }
+        this.valuesArray = valuesArray;
+    }
+
+    public Vector(Vector vector) {
+        n = vector.n;
+
+        valuesArray = vector.valuesArray;
+    }
+
+    public Vector(double[] valuesArray) {
+        if (valuesArray.length == 0) {
+            throw new ArrayIndexOutOfBoundsException("n can’t be = 0");
+        }
+        n = valuesArray.length;
+
+        this.valuesArray = valuesArray;
+    }
+
+    public Vector(int n, double[] array) {
+
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public double[] getValluesArray() {
+        return valuesArray;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public void setValluesArray(double[] valluesArray) {
+        this.valuesArray = valluesArray;
+    }
 }
--- Vector/src/ru/academits/koloskova/vector/main/Main.java
+++ Vector/src/ru/academits/koloskova/vector/main/Main.java
@@ -1,4 +1,18 @@
 package ru.academits.koloskova.vector.main;
 
+import ru.academits.koloskova.vector.Vector;
+
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        double[] array = {1,2,3,4,5};
+        Vector vector = new Vector(array);
+
+//        Vector vector = new Vector(4);
+        Vector vector1 = new Vector(vector);
+
+        System.out.println(Arrays.toString(vector.getValluesArray()));
+        System.out.println(Arrays.toString(vector1.getValluesArray()));
+    }
 }
