0e1422cc838a5b416b5b6767e4a17c4db2b2aa88
klskvmr <koloskova.marii@gmail.com>
2019-08-04 22:56:16 +0700

    work on bugs2

--- Shapes/src/ru/academits/koloskova/shapes/comparator/AreaComparator.java
+++ Shapes/src/ru/academits/koloskova/shapes/comparator/AreaComparator.java
@@ -7,11 +7,6 @@ import java.util.Comparator;
 public class AreaComparator implements Comparator<Shape> {
     @Override
     public int compare(Shape shape1, Shape shape2) {
-        if (shape1.getArea() < shape2.getArea()) {
-            return -1;
-        } else if (shape1.getArea() == shape2.getArea()) {
-            return 0;
-        }
-        return 1;
+        return Double.compare(shape1.getArea(), shape2.getArea());
     }
 }
--- Shapes/src/ru/academits/koloskova/shapes/comparator/PerimeterComparator.java
+++ Shapes/src/ru/academits/koloskova/shapes/comparator/PerimeterComparator.java
@@ -7,11 +7,6 @@ import java.util.Comparator;
 public class PerimeterComparator implements Comparator<Shape> {
     @Override
     public int compare(Shape shape1, Shape shape2) {
-        if (Double.compare(shape1.getPerimeter(), shape2.getPerimeter()) < 0) {
-            return -1;
-        } else if (Double.compare(shape1.getPerimeter(),shape2.getPerimeter()) == 0) {
-            return 0;
-        }
-        return 1;
+        return Double.compare(shape1.getPerimeter(), shape2.getPerimeter());
     }
 }
