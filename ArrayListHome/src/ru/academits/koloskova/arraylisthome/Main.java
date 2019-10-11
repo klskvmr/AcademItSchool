0db8542e32446acffa681ea9fcb52974a853eebb
Мария Колоскова <maria@MBP-Maria.cn.ru novotelecom.ru>
2019-10-11 15:44:25 +0700

    ArrayListHome for first check

--- ArrayListHome/src/ru/academits/koloskova/arraylisthome/Main.java
+++ ArrayListHome/src/ru/academits/koloskova/arraylisthome/Main.java
@@ -15,29 +15,25 @@ public class Main {
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
         }
+
         System.out.println("Task №1: " + stringList);
 
-//        Iterator<Integer> iterator = integerList1.iterator();
-//        for (Iterator<Integer> i = integerList1.iterator(); i.hasNext(); ) {
-//            Integer number = i.next();
-//            if (number % 2 == 0) {
-//                i.remove();
-//            }
-//        }
-
         List<Integer> integerList1 = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
         integerList1.removeIf(number -> number % 2 == 0);
 
         System.out.println("Task №2: " + integerList1);
 
         List<Integer> integerList2 = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
         List<Integer> integerList3 = new ArrayList<>();
+
         for (Integer number : integerList2) {
-            if (integerList3.contains(number)){
+            if (integerList3.contains(number)) {
                 continue;
             }
             integerList3.add(number);
         }
+
         System.out.println("Task №3: " + integerList3);
     }
 }
